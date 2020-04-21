30a17238cb697c196bfa36cc7b196bdb1603af8b
hyphenrf <hyphens@pm.me>
2020-04-22 00:16:28 +0200

    Don't trash people's logs

--- examples/bat_watcher.sh
+++ examples/bat_watcher.sh
@@ -7,24 +7,25 @@
 #
 
 hb=/usr/local/bin/hb
+crit=15
 
 acpi=/usr/bin/acpi
 notify=/usr/bin/notify-send
 grep=/bin/grep
 
 
-if $acpi -b |$grep -v Charging
+if $acpi -b |$grep -qv Charging
 then
 	percent=$( $acpi -b |cut -d, -f2 |tr -d '% ' )
 	
-	[ $percent -gt 15 ] || {
+	[ $percent -gt $crit ] || {
 		
 		$notify -u critical -i gnome-power-manager "Bat Watcher" \
-		"Battery critical: $percent%, hibernating in 10 seconds";
+		"Battery critical: $percent%, hibernating in 10 seconds" ;
 
 		sleep 10;
 
-		if $acpi -b |$grep -v Charging; 
+		if $acpi -b |$grep -qv Charging; 
 		then $hb; 
 		fi;
 	}
