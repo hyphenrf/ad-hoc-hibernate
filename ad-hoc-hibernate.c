9e50695bbbf82a8e76aee591dddc41206f9e9fbc
hyphenrf <hyphens@pm.me>
2020-05-15 10:32:19 +0200

    readability, don't be cs grad meme

--- ad-hoc-hibernate.c
+++ ad-hoc-hibernate.c
@@ -10,44 +10,41 @@
 #include <stdio.h>
 #include <string.h>
 
-#define STATEFILE "/sys/power/state"
-#define STATETYPE "disk"
-
-
-void err(char *str)
-{
-	fprintf(stderr, "\e[31mError: %s \e[0m\n", str);
-}
+#include "ad-hoc-hibernate.h"
 
 int main()
 {
 	FILE *state;
 	char buf[32]; /* There are at most "mem freeze disk standby" entries
-								** in /sys/power/state .. Which are in total 24 bytes */
-	
+	              ** in /sys/power/state .. Which are in total 24 bytes */
+
+
 	state = fopen(STATEFILE, "r");
 
+	/* File exists? read it and rewind */
 	if (state) {
 		fgets(buf, 32, state);
 		rewind(state);
-		
-		if (strstr(buf, STATETYPE) != NULL) {
-			if (freopen(NULL, "w", state) != NULL) {
-				puts("Suspending to disk...");
-				fputs(STATETYPE, state);
-				fclose(state);
-
-			} else {
-				err("can't write to "STATEFILE". Are you privileged?");
-				goto error;
-			}	
-		} else {
-			err("kernel reported that this system doesn't support suspend to disk.");
-			fclose(state);
-			goto error;
-		}
 	} else {
 		err(""STATEFILE" does not exist.");
+		goto error;
+	}
+
+	/* File doesn't STATETYPE in it? bail */
+	if (strstr(buf, STATETYPE) == NULL) {
+		err("kernel reported that this system doesn't support suspend to disk.");
+		fclose(state);
+		goto error;
+	}
+
+
+	/* file writable? execute successfully */
+	if (freopen(NULL, "w", state) != NULL) {
+		puts("Suspending to disk...");
+		fputs(STATETYPE, state);
+		fclose(state);
+	} else {
+		err("can't write to "STATEFILE". Are you privileged?");
 		goto error;
 	}
 
@@ -56,4 +53,9 @@ int main()
 
 error:
 	return EXIT_FAILURE;
+}
+
+void err(char *str)
+{
+	fprintf(stderr, "\e[31mError: %s \e[0m\n", str);
 }
---
+++ ad-hoc-hibernate.h
@@ -0,0 +1,9 @@
+#ifndef AD_HOC_HIBERNATE_H
+#define AD_HOC_HIBERNATE_H
+
+#define STATEFILE "/sys/power/state"
+#define STATETYPE "disk"
+
+void err(char *str);
+
+#endif
